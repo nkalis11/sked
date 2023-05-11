5101e2286fc3652a29989a65653f0f35ff5fd088
nkalis11 <77913654+nkalis11@users.noreply.github.com>
2023-05-10 18:06:45 -1000

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,6 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div>{!user.isSignedIn && <SignInButton />}{!!user.isSignedIn && <UserButton />}</div>
-
       </main>
     </>
   );
