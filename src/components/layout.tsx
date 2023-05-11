0fdbb20b16b7e96e6255c2be924794e9a4cb7f80
nkalis11 <77913654+nkalis11@users.noreply.github.com>
2023-05-10 21:46:57 -1000

    Update layout.tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,6 @@
 import { Fragment, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
-import pageLinks from "~/pages/components/Navigation/pagelinks";
+import pageLinks from "./Navigation/pagelinks";
 import {
     Bars3Icon,
     XMarkIcon
