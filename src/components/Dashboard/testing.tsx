f51064bd711c498a4858b307f7adf4392224d1a1
nkalis11 <77913654+nkalis11@users.noreply.github.com>
2023-05-14 22:06:50 -1000

    fixed modal

--- src/components/Dashboard/testing.tsx
+++ src/components/Dashboard/testing.tsx
@@ -1,15 +1,40 @@
 import { MaintenanceCard, PrismaClient } from "@prisma/client";
-import { useEffect, useState } from "react";
+import { useEffect, useState, Fragment } from "react";
+import { Listbox, Transition } from '@headlessui/react'
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
 import { api } from "~/utils/api";
 import React from "react";
 import { clerkClient } from "@clerk/nextjs/server";
 import { useMutation } from "@tanstack/react-query";
+import { List } from "@tremor/react";
+import MaintModal from "../Modals/maintModal";
+
+function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(' ')
+}
 
 export default function Testing() {
     const cardsQuery = api.maintenanceCard.getAll.useQuery();
     const usersQuery = api.maintenanceCard.getAllUsers.useQuery();
     const users = usersQuery.data || [];
     const [maintenanceCards, setMaintenanceCards] = useState<MaintenanceCard[]>([]);
+    const [selectedCard, setSelectedCard] = useState<MaintenanceCard | null>(null);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+      const handleCardClick = (card: MaintenanceCard, event: React.MouseEvent) => {
+        const target = event.target as HTMLElement;
+        const isCardTitleCell = target.classList.contains('card-title-cell');
+      
+        if (isCardTitleCell) {
+          setSelectedCard(card);
+          setIsModalOpen(true);  // <-- Set the modal to open
+        }
+    };
+
+    const closeModal = () => {
+        setSelectedCard(null);
+        setIsModalOpen(false);  // <-- Set the modal to close
+    };
 
     useEffect(() => {
       if (cardsQuery.data) {
@@ -44,7 +69,7 @@ export default function Testing() {
       };
 
     return (
-        <>
+        <div className="relative">
         <div className="px-4 sm:px-6 lg:px-8">
             {/* Card Header */}
             <div className="sm:flex sm:items-center">
@@ -73,7 +98,7 @@ export default function Testing() {
                             <tbody className="bg-white">
                                 {maintenanceCards.map((card) => (
                                     <tr key={card.id} className="even:bg-gray-50">
-                                        <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">{card.Title}</td>
+                                        <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3 card-title-cell hover:text-blue-700 cursor-pointer" onClick={(event) => handleCardClick(card, event)}>{card.Title}</td>
                                         <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{card.System}</td>
                                         <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{card.Subsystem}</td>
                                         <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{card.Equipment}</td>
@@ -86,30 +111,66 @@ export default function Testing() {
                                             }
                                         </td>
                                         <td>
-                                            <select
-                                                value={card.assigneeId || ""}
-                                                onChange={(e) =>  {
-                                                    const newUserId = e.target.value;
-                                                    handleAssigneeChange(card.id, newUserId).catch((error) => {
+                                            <Listbox value={card.assigneeId || ""} onChange={(value) => {
+                                                const ignorePromise = async () => {
+                                                    try {
+                                                        await handleAssigneeChange(card.id, value);
+                                                    } catch (error) {
                                                         console.error('Failed to update assignee:', error);
-                                                    });
-                                                }}
-                                            >
-                                                {users.map((user) => (
-                                                    <option key={user.id} value={user.id}>
-                                                        {user.firstName} {user.lastName} 
-                                                    </option>
-                                                ))}
-                                            </select>
+                                                    }
+                                                };
+                                                void ignorePromise();
+                                            }}>
+                                                {({ open }) => (
+                                                    <>
+                                                        <div className="mt-2">
+                                                        <Listbox.Button className="relative w-64 cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                                            <span className="block truncate">Assign New User</span>
+                                                            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                                                                <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                                                            </span>
+                                                        </Listbox.Button> 
+                                                        {open && (
+                                                          <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-64 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                                                          <Transition
+                                                            show={open}
+                                                            leave="transition ease-in duration-100"
+                                                            leaveFrom="opacity-100"
+                                                            leaveTo="opacity-0"
+                                                          >
+                                                            {users.map((user) => (
+                                                              <Listbox.Option key={user.id} value={user.id} className={({ active }) =>
+                                                                classNames(
+                                                                  active ? 'bg-indigo-600 text-white' : 'text-gray-900',
+                                                                  'relative cursor-default select-none py-2 pl-8 pr-4'
+                                                                )
+                                                              }>
+                                                                {user.firstName} {user.lastName} 
+                                                              </Listbox.Option>
+                                                            ))}
+                                                          </Transition>
+                                                        </Listbox.Options>
+                                                        
+                                                            
+                                                        )}
+                                                        </div>
+                                                    </>
+
+                                                )}
+                                            </Listbox>  
                                         </td>
                                     </tr>
                                 ))}
-                            </tbody>    
+                            </tbody>  
                         </table>
+                         {/* Render Modal*/ }  
+                         {selectedCard && (
+                            <MaintModal card={selectedCard} onClose={closeModal} open={isModalOpen} />
+                            )}
                     </div>
                 </div>
             </div>
         </div>
-        </>
+        </div>
     );
 }
