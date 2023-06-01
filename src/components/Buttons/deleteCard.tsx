00ae43bf9f391b648fa4262c0dca5365fcf39971
nkalis11 <77913654+nkalis11@users.noreply.github.com>
2023-06-01 16:47:29 -0500

    Update deleteCard.tsx

--- src/components/Buttons/deleteCard.tsx
+++ src/components/Buttons/deleteCard.tsx
@@ -24,7 +24,9 @@ interface DeleteCardButtonProps {
 
         return (
             <button 
-                onClick={() => handleClick(cardId)}
+            onClick={async () => {
+                await handleClick(cardId);
+            }}
                 className="rounded bg-indigo-600 px-2 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >Delete
             </button>
