b3ae1e9d895ceba38349dff1b7ea7e5afa99bc71
nkalis11 <77913654+nkalis11@users.noreply.github.com>
2023-06-01 16:56:05 -0500

    Update deleteCard.tsx

--- src/components/Buttons/deleteCard.tsx
+++ src/components/Buttons/deleteCard.tsx
@@ -24,17 +24,7 @@ interface DeleteCardButtonProps {
 
         return (
             <button 
-            onClick={() => {
-                handleClick(cardId)
-                  .then(() => {
-                    // Handle successful deletion if needed
-                  })
-                  .catch(err => {
-                    // Handle error
-                    console.error("Error deleting card:", err);
-                    // You can display an error message to the user or perform any other necessary actions
-                  });
-              }}
+            
                 className="rounded bg-indigo-600 px-2 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >Delete
             </button>
