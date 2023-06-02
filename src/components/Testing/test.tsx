9c70d2925da369c82a7df8ba4d01a09a0161680e
nkalis11 <77913654+nkalis11@users.noreply.github.com>
2023-06-01 19:36:35 -0500

    deployment fix

--- src/components/Testing/test.tsx
+++ src/components/Testing/test.tsx
@@ -95,4 +95,4 @@ export default function Test() {
         </div>
         </>
     )
-};
+}
--- src/pages/dashboard/tasks/index.tsx
+++
@@ -1,9 +0,0 @@
-import { NextPage } from 'next';
-
-const Test: NextPage = () => {
-    return (
-        <div>
-            <h1>Test</h1>
-        </div>
-    )
-}
